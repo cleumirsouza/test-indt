084d8d0a4dbcb5bad1d54b2705bcb5a6ed8d9675
Cleumir Souza <Cleumir.Souza@fit-tecnologia.org.br>
2018-12-28 17:12:46 -0400

    [backend] - adjustments to deploy

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * Author: Cleumir Souza
- * Date: 28/12/2018
- * @description Starting application in development mode.
- */
-if (process.env.NODE_ENV !== 'production') {
-    require('file-loader!../index.html')
-}
